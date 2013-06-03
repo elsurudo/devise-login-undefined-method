497338a57052ff5bcf7ad786231f3653f3799a9e
Ernest Surudo <ernest.surudo@gmail.com>
2013-06-03 14:35:11 +0200

    Formatting fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def after_sign_in_path_for(resource)
-  		stored_path = stored_location_for(resource)
-
-  		return stored_path if stored_path
-
-  		if resource == :admin
-  		  root_path
-  		end
-  	end
-
-  	def after_sign_out_path_for(resource)
-  		if resource == :admin
-  			new_admin_session_path
-  		end
-  	end
+    stored_path = stored_location_for(resource)
+  
+    return stored_path if stored_path
+  
+    if resource == :admin
+      root_path
+    end
+  end
+  
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    end
+  end
 end
